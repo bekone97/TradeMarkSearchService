b680318970e3aaf03c14787868865835809ecd14
bekone97 <myachienergo@mail.ru>
2023-07-06 07:21:41 +0300

    Added objectMapper bean.

--- src/main/java/by/intexsoft/trademarksearchservice/config/Config.java
+++ src/main/java/by/intexsoft/trademarksearchservice/config/Config.java
@@ -2,6 +2,7 @@ package by.intexsoft.trademarksearchservice.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.mongock.runner.springboot.EnableMongock;
@@ -17,29 +18,36 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @EnableMongock
 public class Config {
 
-    @Bean
-    public XmlMapper xmlMapper() {
-        return XmlMapper.builder()
-                .defaultUseWrapper(false)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
-                .addModule(new JavaTimeModule())
-                .build();
-    }
+  @Bean
+  public ObjectMapper objectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    return objectMapper;
+  }
 
-    @Bean
-    public MessageSource validationMessageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:messages/validation");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
+  @Bean
+  public XmlMapper xmlMapper() {
+    return XmlMapper.builder()
+            .defaultUseWrapper(false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+            .addModule(new JavaTimeModule())
+            .build();
+  }
 
-    @Bean
-    public LocalValidatorFactoryBean localValidator() {
-        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
-        bean.setValidationMessageSource(validationMessageSource());
-        return bean;
-    }
+  @Bean
+  public MessageSource validationMessageSource() {
+    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+    messageSource.setBasename("classpath:messages/validation");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
+
+  @Bean
+  public LocalValidatorFactoryBean localValidator() {
+    LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+    bean.setValidationMessageSource(validationMessageSource());
+    return bean;
+  }
 
 }
